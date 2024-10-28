91f346c5aebc4f96be01919c516c6a02022c866e
AlexMacBook <alex.moik@gmx.at>
2024-10-28 12:37:01 +0100

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit if necessary
-  }
+  base: '/nostr-nips/',
+  plugins: [react()]
 });
