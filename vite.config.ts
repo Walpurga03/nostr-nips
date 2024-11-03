94133f6de3904e37e7f23ac7ac5b7c399f28a107
alex <alex.moik@gmx.at>
2024-11-03 11:08:19 +0100

    Aufteilen der Abhängigkeiten

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,22 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/nostr-nips/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react-vendor';
+            if (id.includes('lodash')) return 'lodash-vendor';
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600, // Erhöhtes Limit für Chunk-Warnungen
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 });
